133eec332344a014d7e71d0de5ddb1f81226d463
Ivo Zivkov <izivkov@gmail.com>
2022-09-07 21:28:22 -0400

    WIP

--- app/src/main/java/org/avmedia/remotevideocam/utils/Utils.kt
+++ app/src/main/java/org/avmedia/remotevideocam/utils/Utils.kt
@@ -63,38 +63,4 @@ object Utils {
         val ipAddr = InetAddress.getByAddress(byteIp).hostAddress
         return ipAddr
     }
-
-    fun getMyIP_ORIG(): String? {
-        val useIPv4 = true
-        try {
-            val interfaces: List<NetworkInterface> =
-                Collections.list(NetworkInterface.getNetworkInterfaces())
-            for (intf in interfaces) {
-                val addrs = Collections.list(intf.inetAddresses)
-                for (addr in addrs) {
-                    if (!addr.isLoopbackAddress) {
-                        val sAddr = addr.hostAddress
-                        // boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
-                        val isIPv4 = sAddr.indexOf(':') < 0
-                        if (useIPv4) {
-                            if (isIPv4) {
-                                return sAddr
-                            }
-                        } else {
-                            if (!isIPv4) {
-                                val delim = sAddr.indexOf('%') // drop ip6 zone suffix
-                                return if (delim < 0) sAddr.uppercase(Locale.getDefault()) else sAddr.substring(
-                                    0,
-                                    delim
-                                ).uppercase(Locale.getDefault())
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            Timber.i("Exception in getIPAddress: $e")
-        } // for now eat exceptions
-        return ""
-    }
 }
